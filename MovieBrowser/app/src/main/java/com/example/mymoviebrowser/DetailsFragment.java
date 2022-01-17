38b6b4c2106537452536bdd9a6439dc6b0fde7dc
alimarangoz <ali.marangoz1999@gmail.com>
2022-01-17 15:50:32 +0300

    CloudDb and Fragment

--- MovieBrowser/app/src/main/java/com/example/mymoviebrowser/DetailsFragment.java
+++ MovieBrowser/app/src/main/java/com/example/mymoviebrowser/DetailsFragment.java
@@ -3,6 +3,7 @@ package com.example.mymoviebrowser;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -59,20 +60,17 @@ public class DetailsFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView txtName = (TextView)view.findViewById(R.id.txtMovieName);
-        txtName.setText(movie.getName());
-        TextView txtYear = (TextView)view.findViewById(R.id.txtYear);
-        txtYear.setText(Integer.toString(movie.getYear()));
-        TextView txtDirector = (TextView)view.findViewById(R.id.txtDirector);
-        txtDirector.setText(movie.getDirector());
-        TextView txtDescription = (EditText)view.findViewById(R.id.txtDescription);
-        txtDescription.setText(movie.getDescription());
-        txtDescription.setEnabled(false);
-        ListView listView = (ListView) view.findViewById(R.id.lstStars);
-        listView.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.array_adapter,
-                movie.getStars().toArray(new String[1])));
+        setMovie(view,movie);
+
     }
 
     public void setMovie(View viewById, Movie movie) {
